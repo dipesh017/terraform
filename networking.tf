fb4bcd373c0906b3c37d75c96d8d005871418c44
dipesh.garg <dipesh.garg@upgrad.com>
2024-02-19 11:21:43 +0400

    DO-50

--- networking.tf
+++ networking.tf
@@ -1,14 +1,14 @@
-resource "aws_vpc" "this" {
-  cidr_block = "10.100.0.0/16"
-  tags = {
-    Name = "tka-vpc"
-  }
-}
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.this.id
   cidr_block = "10.100.1.0/24"
 
   tags = {
     Name = "public-subnet-1"
+  }
+}
+resource "aws_vpc" "this" {
+  cidr_block = "10.100.0.0/16"
+  tags = {
+    Name = "tka-vpc"
   }
 }
