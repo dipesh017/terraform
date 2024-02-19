4ba15c6b9863ae9adcdae63d01b6584334b9b318
dipesh.garg <dipesh.garg@upgrad.com>
2024-02-19 14:22:18 +0400

    DO-50

--- variable.tf
+++ variable.tf
@@ -1,3 +1,9 @@
 variable "vpc_cidr" {
   default = "10.200.0.0/16"
 }
+variable "module_vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+variable "azs" {
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
--- vpc.tf
+++ vpc.tf
@@ -2,9 +2,9 @@ module "vpc" {
   source = "./modules/terraform-aws-vpc-new"
 
   name = "tka-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = var.module_vpc_cidr
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs             = var.azs
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
@@ -18,3 +18,4 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
