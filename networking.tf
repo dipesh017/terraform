42436abe4aa8519a48d550dd7e3da0f128dbe43a
dipesh.garg <dipesh.garg@upgrad.com>
2024-02-19 11:32:22 +0400

    DO-50

--- networking.tf
+++ networking.tf
@@ -6,3 +6,22 @@ resource "aws_subnet" "main" {
     Name = "public-subnet-1"
   }
 }
+
+resource "aws_subnet" "main2" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = "10.100.2.0/24"
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+output "vpc_arn" {
+  value = aws_vpc.this.arn
+}
+output "vpc_main_route_table_id" {
+  value = aws_vpc.this.main_route_table_id
+}
