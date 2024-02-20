7d44d1a75e2e68b3a9238fd5c1979804f1fdfcc7
dipesh.garg <dipesh.garg@upgrad.com>
2024-02-20 13:01:41 +0400

    DO-50

--- day-2/meta_arguments.tf
+++ day-2/meta_arguments.tf
@@ -1,9 +1,10 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.201.0.0/16"
+  cidr_block       = "10.202.0.0/16"
   instance_tenancy = "default"
 
   lifecycle {
     create_before_destroy = true
+    prevent_destroy = true
   }
 
   tags = {
