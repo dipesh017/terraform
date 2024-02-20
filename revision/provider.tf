18e25b48b875f7269419a31ec9cb7ca13336a12e
dipesh.garg <dipesh.garg@upgrad.com>
2024-02-20 09:29:25 +0400

    DO-50

---
+++ revision/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.37.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "terraform-tka"
+    key            = "revision/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+provider "aws" {
+  # Configuration options
+  alias  = "mumbai"
+  region = "ap-south-1"
+}
