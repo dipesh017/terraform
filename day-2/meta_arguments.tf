0b8e1557692f8b9f14d4735a9cffa5e210a85445
dipesh.garg <dipesh.garg@upgrad.com>
2024-02-20 12:56:51 +0400

    DO-50

--- day-2/meta_arguments.tf
+++ day-2/meta_arguments.tf
@@ -1,6 +1,10 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.200.0.0/16"
+  cidr_block       = "10.300.0.0/16"
   instance_tenancy = "default"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags = {
     Name = "tka-vpc"
