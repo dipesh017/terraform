64236c83988f6cb43512113e22da7d4a2b4818d4
dipesh.garg <dipesh.garg@upgrad.com>
2024-02-20 11:29:18 +0400

    DO-50

--- day-2/user.tf
+++ day-2/user.tf
@@ -8,6 +8,14 @@ resource "aws_iam_user" "user" {
 }
 
 
+resource "aws_iam_user" "iamuser" {
+  for_each = toset(var.user_name)
+  name = each.key
+  
+  tags = {
+    Name = each.key
+  }
+}
 
 output "rami_user_id" {
   value = aws_iam_user.user[0].id
